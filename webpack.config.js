06dc4fc1e399beeabfb76885885489d91a1b2f8d
Jare Fagbemi <ofagbemi@stanford.edu>
2017-04-04 22:02:09 -0700

    [build] Build CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 
 module.exports = {
@@ -12,21 +14,50 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, 'public/generated/js'),
+    path: path.join(__dirname, 'public/generated/'),
     filename: 'bundle.js',
   },
   plugins: [
+    new ExtractTextPlugin({ filename: 'main.css', allChunks: true }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       filename: 'vendor.bundle.js',
     }),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules\//,
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                camelCase: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [autoprefixer({ browsers: ['last 2 versions'] })],
+              },
+            },
+            'sass-loader?outputStyle=expanded',
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: ['./client/style/export.scss'],
+              },
+            },
+          ],
+        }),
       },
     ],
   },
